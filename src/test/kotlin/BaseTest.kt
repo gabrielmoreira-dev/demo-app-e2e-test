5ca7d34d2670db66144089a3e268efa79eee67c7
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-01-16 00:30:04 -0300

    Fix constants

--- src/test/kotlin/BaseTest.kt
+++ src/test/kotlin/BaseTest.kt
@@ -11,11 +11,11 @@ import java.nio.file.Paths
 
 open class BaseTest {
     companion object {
-        const val AUTOMATION_NAME = "XCUITest"
         const val APP_DIR = "src/test/resources/MyDemoApp.app"
+        const val AUTOMATION_NAME = "XCUITest"
+        const val DEVICE_NAME = "iPhone 15 Pro"
         const val PLATFORM_NAME = "IOS"
         const val PLATFORM_VERSION = "18.0"
-        const val DEVICE_NAME = "iPhone 15 Pro"
         const val SERVER_URL = "http://127.0.0.1:4723"
     }
 
@@ -32,11 +32,11 @@ open class BaseTest {
 
     @BeforeClass
     protected fun setUpDriver() = XCUITestOptions()
-        .setAutomationName(AUTOMATION_NAME)
         .setApp(Paths.get(System.getProperty("user.dir"), APP_DIR).toAbsolutePath().toString())
+        .setAutomationName(AUTOMATION_NAME)
+        .setDeviceName(DEVICE_NAME)
         .setPlatformName(PLATFORM_NAME)
         .setPlatformVersion(PLATFORM_VERSION)
-        .setDeviceName(DEVICE_NAME)
         .let { driver = IOSDriver(URL(SERVER_URL), it) }
 
     @AfterClass
